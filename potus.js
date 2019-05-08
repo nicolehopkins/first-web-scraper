e536ce9f4bafefa43390b496760551dfce435c98
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-05-07 20:27:47 -0400

    scraping site for president wiki pages

--- potus.js
+++ potus.js
@@ -1,6 +1,13 @@
 const rp = require('request-promise');
+const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
 rp(url)
-  .then((html) => console.log(html))
+  .then((html) => {
+    let wikiUrls = [];
+    for (let i = 0; i < 45; i++) {
+      wikiUrls.push($('big > a', html)[i].attribs.href)
+    }
+    console.log(wikiUrls)
+  })
   .catch(err => console.log(err))
